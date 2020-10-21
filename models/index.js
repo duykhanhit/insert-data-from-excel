35341a91cfc8b7043f5ad3d95cfa538167420f29
duykhanhit <hdkhanhit@gmail.com>
2020-10-22 03:05:30 +0700

    fix timeout connect to db

--- models/index.js
+++ models/index.js
@@ -8,12 +8,21 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: "mysql",
+    pool: {
+      max: 20,
+      min: 0,
+      acquire: 60000000,
+      idle: 10000
+    }
+  }
+);
 
 fs
   .readdirSync(__dirname)
